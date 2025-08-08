b3609c5a82af847ec13196a563db0d72a599affa
chrispeddemors <chrispeddemors@gmail.com>
2025-08-08 19:39:16 +0200

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [{ protocol: 'https', hostname: 'cdn.jouwdomein.com' }],
+    remotePatterns: [{ protocol: 'https', hostname: 'maestro-ai.nl' }],
   },
 };
 export default nextConfig;
